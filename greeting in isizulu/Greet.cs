7016817dfb5e4b6e49d697b43a4d748f4b3fe261
nomvula <nzshabangu95@gmail.com>
2023-09-18 13:12:51 +0200

    Add project files.

---
+++ greeting in isizulu/Greet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeting_in_isizulu
+{
+    class Greet
+    {
+        public static void sayHi()
+            {
+
+            Console.WriteLine("hi ");
+            }
+    }
+}
---
+++ greeting in isizulu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeting_in_isizulu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet.sayHi();
+
+            Console.ReadLine();
+        }
+    }
+}
